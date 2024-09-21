1d24df59d58f72b2decaa30b3ef09315017a2034
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-09-21 15:37:30 +0530

    apply font, create usercard, fetch users and display users list

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,49 @@
+import { Envelope, Link, Phone } from "@phosphor-icons/react";
+import { User } from "../types";
+
+const UserCard = ({ username, email, name, phone, website }: User) => {
+  return (
+    <article className="flex gap-x-2 p-2 items-center text-gray-700 border rounded-md shadow hover:border-gray-300 hover:cursor-pointer md:space-x-5">
+      <img
+        src="https://avatar.iran.liara.run/public"
+        alt={`${name}'s avatar`}
+        className="size-24"
+      />
+
+      <div className="space-y-1 w-full text-sm">
+        <div className="mb-3">
+          <h2 className="text-black font-bold text-base">{name}</h2>
+          <h3 className="text-xs text-gray-500">@{username}</h3>
+        </div>
+
+        <div className="md:flex items-center gap-x-5">
+          <div className="flex items-center">
+            <Envelope
+              size={18}
+              color="gray-600"
+              className="mr-2"
+              weight="bold"
+            />
+            <span className="hover:text-blue-500">{email}</span>
+          </div>
+          <div className="flex items-center">
+            <Phone size={18} color="gray-600" className="mr-2" weight="bold" />
+            <span className="hover:text-blue-500">{phone}</span>
+          </div>
+          <div className="flex items-center">
+            <Link size={18} color="gray-600" className="mr-2" weight="bold" />
+            <a
+              href={`https://${website}`}
+              target="_blank"
+              className="hover:text-blue-500"
+            >
+              {website}
+            </a>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default UserCard;
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import UserCard from "../components/UserCard";
+import { User } from "../types";
+import axios from "axios";
+import { SpinnerGap } from "@phosphor-icons/react";
+
 const Users = () => {
-  return <div>Users</div>;
+  const [users, setUsers] = useState<User[]>([]);
+  const [spin, setSpin] = useState(false);
+  const getAllUsers = async () => {
+    try {
+      setSpin(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSpin(false);
+    }
+  };
+  useEffect(() => {
+    getAllUsers().then((res) => {
+      setUsers(res?.data);
+    });
+  }, []);
+
+  return (
+    <div className=" flex flex-col min-h-screen p-2">
+      <h1 className="text-violet-600 font-extrabold text-3xl mb-6">Users</h1>
+      <div className="space-y-5" style={{ scrollbarWidth: "thin" }}>
+        {spin ? (
+          <div className="text-black font-medium tracking-widest inline-flex items-center text-xl gap-x-5">
+            <span>Loading</span>
+            <SpinnerGap size={50} className="text-violet-500 animate-spin" />
+          </div>
+        ) : (
+          users.map((eachUser) => <UserCard {...eachUser} />)
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
